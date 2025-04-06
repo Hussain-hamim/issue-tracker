d7a8ad718bbf5094ba57b1b17d86d3a47ad5e8d0
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-04-06 21:15:12 +0430

    building an api

---
+++ app/api/issues/route.ts
@@ -0,0 +1,25 @@
+import { prisma } from '@/prisma/client';
+import { NextResponse } from 'next/server';
+import z from 'zod';
+
+const issueSchema = z.object({
+  title: z.string().min(1, { message: 'Title is required' }).max(255),
+  description: z.string().min(1),
+});
+
+export async function POST(request: NextResponse) {
+  const body = await request.json();
+  const validation = issueSchema.safeParse(body);
+
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
+
+  const newIssue = await prisma.issue.create({
+    data: {
+      title: body.title,
+      description: body.description,
+    },
+  });
+
+  return NextResponse.json(newIssue, { status: 201 });
+}

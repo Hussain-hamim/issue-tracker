ed6df43568431888444ccb0850c8d41a4f749702
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-04-06 22:47:31 +0430

    build new issue page

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,16 +1,18 @@
 'use client';
-import { TextField } from '@radix-ui/themes';
+import { Button, TextArea, TextField } from '@radix-ui/themes';
 import React from 'react';
 import { BiSearch } from 'react-icons/bi';
 
 const NewIssuePage = () => {
   return (
-    <div className='max-w-xl'>
+    <div className='max-w-xl space-y-3'>
       <TextField.Root placeholder='Title'>
         <TextField.Slot>
           <BiSearch size={20} />
         </TextField.Slot>
       </TextField.Root>
+      <TextArea placeholder='Description' />
+      <Button>Submit New Issue</Button>
     </div>
   );
 };

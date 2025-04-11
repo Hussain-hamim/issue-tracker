6415c47891e4aaadc82ed715153acfe9f151c183
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-04-11 22:08:47 +0430

    update theme to dark mode, refactor Home component to display latest issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,6 @@
 import React from 'react';
-import Pagination from './components/Pagination';
+import LatestIssues from './LatestIssues';
 
-export default async function Home({
-  searchParams,
-}: {
-  searchParams: Promise<{ page?: string }>;
-}) {
-  // const page = Number((await searchParams).page) || 1; // my solution
-
-  const resolvedParams = await searchParams;
-  const page = Number(resolvedParams.page) || 1; // Default to page 1 if invalid or missing
-  console.log('page', page);
-
-  return <Pagination itemCount={100} pageSize={10} currentPage={page} />;
+export default async function Home() {
+  return <LatestIssues />;
 }
--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,6 @@
 export { default } from 'next-auth/middleware';
 
 export const config = {
-  matcher: ['/issues/new', '/issues/:id+/edit'],
+  // matcher: ['/issues/new', '/issues/:id+/edit'],
+  matcher: ['/issues/:id+/edit'], // protected routes
 };

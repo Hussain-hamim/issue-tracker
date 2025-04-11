79eb31e59d337b7b3cec89907194d8f1f5ef6129
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-04-11 15:12:53 +0430

    build pagination ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import Pagination from './components/Pagination';
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return <Pagination itemCount={101} pageSize={10} currentPage={2} />;
 };
 
 export default HomePage;

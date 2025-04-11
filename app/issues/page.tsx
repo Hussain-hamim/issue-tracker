819de852181062237e26313556fb6e3cdf05e65f
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-04-11 11:37:19 +0430

    fix the bug sorted by

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -19,21 +19,21 @@ export default async function IssuePage({ searchParams }: Props) {
   ];
 
   const orderByParams = (await searchParams).orderBy;
-  const status = (await searchParams).status;
 
-  // const statues = Object.values(Status);
-  // const status = statues.includes((await searchParams).status)
-  //   ? (await searchParams).status
-  //   : undefined;
+  const statues = Object.values(Status);
+  const status = statues.includes((await searchParams).status)
+    ? (await searchParams).status
+    : undefined;
+
+  const orderBy = columns.map((column) => column.value).includes(orderByParams)
+    ? { [orderByParams]: 'asc' }
+    : undefined;
 
   const issues = await prisma.issue.findMany({
     where: { status },
-    orderBy: {
-      // [orderByParams]: 'asc', /**TODO: fix the bug */
-    },
+    orderBy: orderBy /**TODO: fixed*/,
   });
   // await delay(2000);
-  // console.log(orderByParams);
 
   return (
     <div>
